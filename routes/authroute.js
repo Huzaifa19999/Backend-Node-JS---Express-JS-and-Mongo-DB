3bdae5509105643b96650fa31079f582332230c1
Huzaifa19999 <huzaifabhai10@gmail.com>
2024-08-31 17:30:37 +0500

    Backend JS

--- routes/authroute.js
+++ routes/authroute.js
@@ -2,8 +2,8 @@ const express = require('express');
 const AuthController = require('../controllers/authcontroller');
 const route = express.Router();
 
+route.post('/signup',AuthController.signup)
 route.post('/login',AuthController.login)
-route.post('/login',AuthController.signup)
 route.get('/checkauth',AuthController.checkauth)
 
 module.exports = route;
